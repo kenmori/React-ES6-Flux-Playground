d56d0cfaab174b99c4ecab73dd1d29e2e448035e
Kenji Morita <kennjimoritata@yahoo.co.jp>
2015-12-18 08:22:18 +0900

    Observer　

--- Hello/js/app.js
+++ Hello/js/app.js
@@ -1,3 +1,9 @@
 React.render(
  React.createElement("h1", null, "Hello, world"), document.body
  );
+
+
+ o = Object.create(Object.prototype, {
+  foo: { writable: false, configurable: true, value : "fafa"},
+  bar : {configurable: false, get: funciton(){return 10}, set : function(value){console.log(value)}}
+    });
