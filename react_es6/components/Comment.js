82058e03ca0338aa0cd9812a04d4ffb8ed1c133e
kenji <kennjimoritata@yahoo.co.jp>
2016-01-24 19:08:15 +0900

    add Markdown

---
+++ react_es6/components/Comment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Marked from 'marked'
+
+export default class Comment extends React.Component {
+ render() {
+   var rawMarkup = Marked(this.props.children.toString(), {sanitize: true});
+   return (
+    <div className='comment'>
+      <h3 className='commentAuthor'>
+        {this.props.author}
+      </h3>
+      <span dangerouslySetInnerHTML={{__html: rawMarkup}} />
+    </div>
+  );
+ }
+}
