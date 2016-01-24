1241638fa0dc3605181a0295d5d98a142a454963
kenji <kennjimoritata@yahoo.co.jp>
2016-01-24 19:21:33 +0900

    pass data to child component

--- react_es6/components/CommentList.js
+++ react_es6/components/CommentList.js
@@ -3,11 +3,12 @@ import Comment from './Comment'
 //When you pass value to children, read children file
 export default class CommentList extends React.Component {
   render() {
+    var commentNodes = this.props.data.map((comment)=> {
+      return (<Comment author={comment.author}>{comment.text}</Comment>);
+    });
     return (
       <div className="commentList">
-        {/* set attribue 'author, and then pass value' */}
-        <Comment author='kenji morita'>This is one comment</Comment>
-        <Comment author='takehiro morita'>This is two comment</Comment>
+        {commentNodes}
       </div>
     );
   }
--- react_es6/index.js
+++ react_es6/index.js
@@ -1,7 +1,11 @@
 import React from 'react'
 import CommentBox from './components/CommentBox'
 
+var data = [
+  {author : 'kenji', text: 'This is one comment'},
+  {author : 'morita', text: 'This is *author* comment'}
+];
 React.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('container')
 );
