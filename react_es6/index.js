dff3b3d1e142d5ed42f5688d09cc79353c5a8e92
kenji <kennjimoritata@yahoo.co.jp>
2016-01-24 17:41:12 +0900

    create CommentBox Class

---
+++ react_es6/index.js
@@ -0,0 +1,18 @@
+import React from 'react'
+class CommentBox extends React.Component {
+  constructor(props){
+    super(props);
+  }
+  render(){
+    return (
+      <div className="commentBox">
+      Hello, world I am CommentBox
+      </div>
+    );
+  }
+}
+
+React.render(
+  <CommentBox />,
+  document.getElementById('container')
+);
