0b753749990b8c694823b1be54b24987dbbe3f22
kenji <kennjimoritata@yahoo.co.jp>
2016-02-04 21:12:35 +0900

    add function on submit order to handle

--- react_es6/components/CommentForm.js
+++ react_es6/components/CommentForm.js
@@ -6,12 +6,13 @@ export default class CommentForm extends React.Component {
     var author = React.findDOMNode(this.refs.author).value.trim();
     var text = React.findDOMNode(this.refs.text).vale.trim();
     if(!text || !author) return;
+    this.props.onCommentSubmit({author: author, text: text});
     React.findDOMNode(this.refs.author).value = '';
     React.findDOMNode(this.refs.text).value = '';
   }
   render() {
     return (
-      <form className='commentForm'>
+      <form className='commentForm' onSubmit={this.handleSubmit.bind(this)}>
         <input type="text" placeholder="Youre name" />
         <input type="text" placeholder="Say something..." />
         <input type="submit" value="Post" />
