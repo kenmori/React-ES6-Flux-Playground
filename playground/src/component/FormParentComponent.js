de5b1575b652a3d7926f6d8870adb2b8a56a20bd
kenji <kennjimoritata@yahoo.co.jp>
2016-04-09 20:35:23 +0900

    on the way 'form create'

--- playground/src/component/FormParentComponent.js
+++ playground/src/component/FormParentComponent.js
@@ -1,42 +1,44 @@
 import React from 'react';
-
+import FormChildComponent from './FormChildComponent.js';
 export default class FormParentComponent extends React.Component {
   constructor(props){
       super(props);
+      this.state = {
+        name : '',
+        age : null,
+        country: ''
+      };
+      this._getName = this._getName.bind(this);
   }
-
+  _getName(e) {
+    this.setState({
+      name : e.target.value
+    });
+  }
   render() {
       {(()=>{
-      if(this.props.renderType === 0) {
-          return(
-              <p className='mt5 tagStatus'>
-                <span className='gray1 fs11'>時間外</span>
-              </p>
-          );
-      } else if(this.props.renderType === 1) {
-          return(
-              <p className='mt5 tagStatus type1 center'>
-                <span className='white fs11'>待機中</span>
-              </p>
-          );
-      } else if(this.props.renderType === 2) {
-          return(
-              <p className='mt5 tagStatus type2 center'>
-                <span className='white fs11'>鑑定中</span>
-              </p>
-          );
-      } else if(this.props.renderType === 3) {
-          return(
-              <p className='mt5 tagStatus type3 center'>
-                <span className='white fs11'>予約受付中</span>
-              </p>
-          );
-      }
-    })();
-    }
+
+      })();}
     return(
         <div>
-          <button onClick={this.props.func}>add</button>
+          <h3>Register form</h3>
+          <form action="GET">
+              <label name="name" value="名前" />
+                <input
+                   onChange={this._getName}
+                   type="text"
+                   name="name"
+                   ref="name"/>
+                <p>{this.state.name}</p>
+              <label name="age" value="生年月日" />
+                <input type="text" name="age" ref="age" />
+              <select name="country" id="contry">
+              </select>
+              <div>
+              </div>
+              <button type="submit">送信</button>
+          </form>
+          <FormChildComponent num={1} />
         </div>
     );
   }
