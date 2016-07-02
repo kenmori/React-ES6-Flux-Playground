d2bf6a08b0ab4838012f59848e8154d58030afb1
kenji <kennjimoritata@yahoo.co.jp>
2016-07-02 22:45:21 +0900

    fix order

--- playground/src/component/FormParentComponent.js
+++ playground/src/component/FormParentComponent.js
@@ -53,35 +53,39 @@ export default class FormParentComponent extends React.Component {
   render() {
     return(
         <div>
+        <FormChildComponent num={1} />
+
+          <h2>ここから下調整中(2016/0702)</h2>
+              <hr />
           <AddList
-            addCountToParent={this.addCountFromParent}
-            count={this.state.count} />
-          <hr />
+            addCountToParent={this.addCountFromParent} />
+            <hr />
+
           <h2>【WIP】Register form</h2>
-          <CommentComponent data={this.props.data} />
+      count={this.state.count} />
           <form>
-              <label name='name' value='名前' />
-                <input
-                   onChange={this._getName}
-                   type='text'
+          <label name='name' value='名前' />
+          <CommentComponent data={this.props.data} />
+          <input
+      ref='name' />
+          <p>{this.state.name}</p>
+      onChange={this._getName}
+      type='text'
                    name='name'
-                   ref='name' />
-                <p>{this.state.name}</p>
-              <label name='age' value='生年月日' />
-                <input type='text' name='age' ref='age' />
-              <select name='country' id='contry'>
-              </select>
-              {/*<div
-                onDragOver={this.handleDragOver}
-                onDragEnter={this.handleDragEnter}
-                onDragLeave={this.handleDragLeave}
-                onDrop={this.handleDrop}
-                >
-              </div>*/}
-              <AnswerRadioInput />
-              <button className='btn btn-primary' onClick={this.handleSaveClicked}>保存</button>
+          <label name='age' value='生年月日' />
+          <input type='text' name='age' ref='age' />
+          <select name='country' id='contry'>
+          </select>
+          {/*<div
+               onDragOver={this.handleDragOver}
+               onDragEnter={this.handleDragEnter}
+               onDragLeave={this.handleDragLeave}
+               onDrop={this.handleDrop}
+               >
+               </div>*/}
+          <AnswerRadioInput />
+          <button className='btn btn-primary' onClick={this.handleSaveClicked}>保存</button>
           </form>
-          <FormChildComponent num={1} />
         </div>
     );
   }
